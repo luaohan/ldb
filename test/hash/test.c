1c6860c0619bad8cd54b68b12e17422a0b316fa3
yangaofeng <dxxygf@163.com>
2015-01-15 10:56:39 +0800

    reimpliment test/hash/test.c

--- test/hash/test.c
+++ test/hash/test.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
-static int hash_function_seed = 5381;
-unsigned int HashFunction(const unsigned char *buf, int len);
-int select_server(int hash_value);
-int server1[9] = {1, 1, 1, -1, -1, -1, -1, -1, -1};
-int server2[9] = {-1, -1, -1, 1, 1, 1, -1, -1, -1};
-int server3[9] = {-1, -1, -1, -1, -1, -1, 1, 1, 1};
-int main()
-{
-   char *key1 = "a";
 
-   int hash1 = HashFunction(key1, 1) % 9;
-    
-   int server = select_server(hash1);
-    
-   return 0;
-}
-int select_server(int hash_value) {
-    if (server1[hash_value] != -1) {
-        return 1;
-    } else if (server2[hash_value] != -1) {
-        return 2;
-    } else if (server3[hash_value] != -1) {
-        return 3;
+//virtual node count as array size, get from config
+//virtual node number as array index, get from config, must be series
+//server ip as array element
+//such as:
+//array[0] -- 192.168.0.1
+//array[1] -- 192.168.0.1
+//array[3] -- 192.168.0.1
+//array[4] -- 192.168.0.2
+//array[5] -- 192.168.0.2
+//...
+
+//sample
+//virtual node => server ip
+#define VIRUTAL_NODE_COUNT 9
+char g_server[VIRUTAL_NODE_COUNT][16] = {
+    "192.168.0.1", /*virtual node  0, array index 0*/
+    "192.168.0.1", /*virtual node  1, array index 1*/
+    "192.168.0.1", /*virtual node  2*/
+    "192.168.0.2", /*virtual node  3*/
+    "192.168.0.2", /*virtual node  4*/
+    "192.168.0.2", /*virtual node  5*/
+    "192.168.0.3", /*virtual node  6*/
+    "192.168.0.3", /*virtual node  7*/
+    "192.168.0.3"  /*virtual node  8*/
+};
+
+unsigned int DJBHash(const unsigned char *buf, int len) {
+    static const int hash_function_seed = 5381;
+    unsigned int hash = (unsigned int)hash_function_seed;
+    while (len--) {
+        hash = ((hash << 5) + hash) + (tolower(*buf++)); /* hash * 33 + c */
     }
 
-    return -1;
-}
-unsigned int HashFunction(const unsigned char *buf, int len) {
-    unsigned int hash = (unsigned int)hash_function_seed;
-    while (len--)
-        hash = ((hash << 5) + hash) + (tolower(*buf++)); /* hash * 33 + c */
     return hash;
+}
+
+void GetServer(const char *key)
+{
+   int virtual_node = DJBHash((const unsigned char *)key, strlen(key)) % VIRUTAL_NODE_COUNT;
+   const char *server = g_server[virtual_node];
+
+   //for test
+   printf("key: %s, virutal node: %d, server: %s\r\n", key, virtual_node, server);
+}
+
+int main()
+{
+    GetServer("aa");
+    GetServer("bb");
+    GetServer("cc");
+    GetServer("dd");
+    GetServer("ee");
+
+   return 0;
 }
